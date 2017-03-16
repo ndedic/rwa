4c9f0de7b3e7c3c694514c0769008007ec6cf03b
Nedim Dedic <nedim_dedic@yahoo.com>
2017-03-16 22:02:05 +0100

    Initial commit

---
+++ lv01/objects.js
@@ -0,0 +1,18 @@
+/**
+ * Objects
+ */
+
+//add methods
+
+var user = {
+    name: 'John',
+    age: 23,
+    email: 'john@example.com'
+};
+
+var e = 'email';
+
+console.log(user);
+console.log(user.age);
+console.log(user.relatives);
+console.log(user[e]);
